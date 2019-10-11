802e3505c4afd35ea0ed9a7654a0aa33d3095391
Sam <sam.nguyen.1223@gmail.com>
2019-10-11 15:34:49 -0400

    Add input parser

---
+++ parser.hpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> parseInput(string input) {
+  stringstream ss(input);
+  string token;
+  vector<string> tokens;
+
+  while (getline(ss, input, ' ')) {
+    tokens.push_back(token);
+  }
+
+  return tokens;
+}
