67f7448e51d909edea8882f1ddf496740ddf8bc8
Sam <sam.nguyen.1223@gmail.com>
2019-10-11 17:00:40 -0400

    Add parser to testing

--- Shell/test.cpp
+++ Shell/test.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 int main() {
+  cout << "Prompt:" << endl;
   printPrompt();
   cout << endl;
-  
+
+  cout << "Command Parser (cmd is 'ls -la'):" << endl;
+  Command testCommand = parseCommand("ls -la");
+  cout << testCommand.toString() << endl;
 }
