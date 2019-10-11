20835a3df114dfeeaaa9fd686e2ae3f0f22e0442
Sam <sam.nguyen.1223@gmail.com>
2019-10-11 16:41:06 -0400

    Change prompt to "myshell> "

--- Shell/prompt.hpp
+++ Shell/prompt.hpp
@@ -6,19 +6,20 @@
 using namespace std;
 
 void printPrompt() {
-  // Get PWD from environment variables
-  string pwd = getenv("PWD");
-  stringstream ss(pwd);
-  string curDir;
+  // // Get PWD from environment variables
+  // string pwd = getenv("PWD");
+  // stringstream ss(pwd);
+  // string curDir;
 
-  // Tokenize pwd to get current directory
-  while (getline(ss, curDir, '/')) {
-  }
+  // // Tokenize pwd to get current directory
+  // while (getline(ss, curDir, '/')) {
+  // }
 
-  // Get current time
-  time_t t = time(0);
-  tm* now = localtime(&t);
+  // // Get current time
+  // time_t t = time(0);
+  // tm* now = localtime(&t);
 
-  // Prints time as HH:MM (24 hour format)
-  cout << now->tm_hour << ":" << now->tm_min << " /" << curDir << '/' << endl;
+  // // Prints time as HH:MM (24 hour format)
+  // cout << now->tm_hour << ":" << now->tm_min << " /" << curDir << '/' << endl;
+  cout << "myshell> ";
 }
