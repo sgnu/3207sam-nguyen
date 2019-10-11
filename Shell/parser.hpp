8f00b4a54f70e2f6a33b6a5bdfdf0fad82b0092a
Sam <sam.nguyen.1223@gmail.com>
2019-10-11 16:44:52 -0400

    Parse a single command

--- Shell/parser.hpp
+++ Shell/parser.hpp
@@ -5,20 +5,51 @@
 
 using namespace std;
 
-vector<string> parseInput(string input) {
-  stringstream ss(input);
-  string token;
-  vector<string> tokens;
-
-  while (getline(ss, input, ' ')) {
-    tokens.push_back(token);
-  }
-
-  return tokens;
-}
+enum redirect {
+  NONE,
+  PIPE,
+  INPUT,
+  OUTPUT
+};
 
 class Command {
   public:
     string command;
     vector<string> args;
+    redirect rd;
+
+    void setCommand(string command);
+    void setArgs(vector<string> args);
 };
+
+void Command::setCommand(string command) {
+  this->command = command;
+}
+
+void Command::setArgs(vector<string> args) {
+  this->args = args;
+}
+
+Command parseCommand(string input) {
+  stringstream ss(input);
+  string token;
+  vector<string> tokens;
+
+  Command command;
+
+  while (getline(ss, token, ' ')) {
+    tokens.push_back(token);
+  }
+
+  command.setCommand(tokens.at(0));
+
+  return command;
+}
+
+Command parseInput(string input) {
+  stringstream ss(input);
+  string token;
+  Command command = parseCommand(input);
+
+  return command;
+}
--- Shell/test.cpp
+++ Shell/test.cpp
@@ -1,6 +1,10 @@
 #include "parser.hpp"
 #include "prompt.hpp"
 
+using namespace std;
+
 int main() {
   printPrompt();
+  cout << endl;
+  
 }
