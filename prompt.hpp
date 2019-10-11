db726ebc2046d2969dbd30d97a25795bf4c54f51
Sam <sam.nguyen.1223@gmail.com>
2019-10-11 14:48:23 -0400

    Add current time to prompt

--- prompt.hpp
+++ prompt.hpp
@@ -1,14 +1,22 @@
-#include <iostream>
 #include <stdio.h>
+#include <iomanip>
+#include <iostream>
 #include <sstream>
 
 using namespace std;
 
 void printPrompt() {
+  // Get PWD from environment variables
   string pwd = getenv("PWD");
   stringstream ss(pwd);
   string curDir;
-  while (getline(ss, curDir, '/')) {}
 
-  cout << curDir << endl;
+  // Tokenize pwd to get current directory
+  while (getline(ss, curDir, '/')) {
+  }
+
+  time_t t = time(0);
+  tm* now = localtime(&t);
+
+  cout << now->tm_hour << ":" << now->tm_min << " " << curDir << '/' << endl;
 }
