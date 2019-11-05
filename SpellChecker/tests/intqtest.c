7e93affa5d573ff6b0f5990dd4f2de4abb4107cd
Sam <sam.nguyen.1223@gmail.com>
2019-11-04 19:39:47 -0500

    Add intQ testing

---
+++ SpellChecker/tests/intqtest.c
@@ -0,0 +1,19 @@
+#include "../headers/intqueue.h"
+
+int main(void) {
+  struct IntQ *queue = createIntQueue(4);
+  for (int i = 0; i < 4; i++) {
+    intQPush(queue, i);
+  }
+
+  printf("size: %d front: %d rear: %d\n", queue->size, queue->front, queue->rear);
+  intQPrint(queue);
+
+  intQPop(queue);
+  intQPop(queue);
+
+  printf("size: %d front: %d rear: %d\n", queue->size, queue->front, queue->rear);
+  intQPrint(queue);
+
+  return 0;
+}
